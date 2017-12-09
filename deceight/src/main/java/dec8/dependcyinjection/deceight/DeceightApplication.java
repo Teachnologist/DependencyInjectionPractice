fce84f75362011e37b59e20427589c9722adb755
Jason Triche <jason_triche@rush.edu>
2017-12-08 18:21:53 -0600

    just learned to create new package within directory in IntelliJ. Yay

---
+++ deceight/src/main/java/dec8/dependcyinjection/deceight/DeceightApplication.java
@@ -0,0 +1,12 @@
+package dec8.dependcyinjection.deceight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeceightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeceightApplication.class, args);
+	}
+}
---
+++ deceight/src/test/java/dec8/dependcyinjection/deceight/DeceightApplicationTests.java
@@ -0,0 +1,16 @@
+package dec8.dependcyinjection.deceight;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeceightApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
